c42f14cdf22105feb99bc888ebbaebb5bd72c96e
Isakov Anatolii <likvidator3301@gmail.com>
2021-05-09 13:33:56 +0500

    basic agent

---
+++ Elevator.Agent/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elevator.Agent.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController: Controller
+    {
+        [HttpGet]
+        public ActionResult<string> GetTestAsync()
+        {
+            return Ok(Guid.NewGuid().ToString());
+        }
+    }
+}
